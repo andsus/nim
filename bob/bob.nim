4fdeacd075a3278545ca942b37e21c15337e44a3
Andre Susantin <andre.susantin@google.com>
2021-02-16 10:37:25 -0800

    bob

---
+++ bob/bob.nim
@@ -0,0 +1,18 @@
+import strutils
+
+proc hey*(utterance: string): string =
+    let 
+        isSilence  = utterance.isEmptyOrWhitespace
+        isQuestion = utterance.strip.endsWith("?")
+        isShouting = utterance.contains(Letters) and utterance.toUpperAscii == utterance
+    return 
+        if isSilence:
+            "Fine. Be that way!"
+        elif isQuestion and isShouting:
+            "Calm down, I know what I'm doing!"
+        elif isQuestion:
+            "Sure."
+        elif isShouting: 
+            "Whoa, chill out!" 
+        else:
+            "Whatever."
