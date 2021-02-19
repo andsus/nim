f574de2266445ee720e123c719ceecddf2e542b4
Andre Susantin <andre.susantin@google.com>
2021-02-19 10:02:52 -0800

    bob

--- bob/bob.nim
+++ bob/bob.nim
@@ -5,14 +5,14 @@ proc hey*(utterance: string): string =
         isSilence  = utterance.isEmptyOrWhitespace
         isQuestion = utterance.strip.endsWith("?")
         isShouting = utterance.contains(Letters) and utterance.toUpperAscii == utterance
-    return 
-        if isSilence:
-            "Fine. Be that way!"
-        elif isQuestion and isShouting:
-            "Calm down, I know what I'm doing!"
-        elif isQuestion:
-            "Sure."
-        elif isShouting: 
-            "Whoa, chill out!" 
-        else:
-            "Whatever."
+     
+    if isSilence:
+        "Fine. Be that way!"
+    elif isQuestion and isShouting:
+        "Calm down, I know what I'm doing!"
+    elif isQuestion:
+        "Sure."
+    elif isShouting: 
+        "Whoa, chill out!" 
+    else:
+        "Whatever."
